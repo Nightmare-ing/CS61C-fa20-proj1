6c4107d1aadb25a1997e96894614b1dac1d79484
Qingsong Hu <kingsanhu@gmail.com>
2023-07-19 09:09:40 +0800

    Finish Part A1
    Debugged Info
    - forget to update pixel at the end of each row in line 71
    - forget to free image instance, each Color row in image->image

--- imageloader.c
+++ imageloader.c
@@ -25,17 +25,61 @@
 //Make sure that you close the file with fclose before returning.
 Image *readData(char *filename) 
 {
-	//YOUR CODE HERE
+	Image *image_st = (Image *) malloc(sizeof(Image));
+    FILE *fp = fopen(filename, "r");
+    char identifier[20];
+    uint32_t cols = 0, rows = 0;
+    uint32_t index_col = 0, index_row = 0;
+    int R, G, B;
+    int max_value;
+    Color *pixel = NULL;
+
+    fscanf(fp, "%s %d %d %d", identifier, &cols, &rows, &max_value);
+    
+    image_st->cols = cols;
+    image_st->rows = rows;
+    image_st->image = (Color **) malloc(sizeof(Color *) * rows);
+
+    for (index_row = 0; index_row < rows; ++index_row) {
+        Color *row = (Color *) malloc(sizeof(Color) * cols);
+        for (index_col = 0; index_col < cols; ++index_col) {
+            pixel = &row[index_col];
+            fscanf(fp, "%d %d %d", &R, &G, &B);
+            pixel->R = (uint8_t) R;
+            pixel->G = (uint8_t) G;
+            pixel->B = (uint8_t) B;
+        }
+        image_st->image[index_row] = row;
+    }
+
+    fclose(fp);
+    return image_st;
 }
 
 //Given an image, prints to stdout (e.g. with printf) a .ppm P3 file with the image's data.
 void writeData(Image *image)
 {
-	//YOUR CODE HERE
+    Color *pixel = NULL;
+	printf("P3\n%d %d\n255\n", image->cols, image->rows);
+    uint32_t i = 0, j = 0;
+
+    for (i = 0; i < image->rows; ++i) {
+        for (j = 0; j < image->cols - 1; ++j) {
+            pixel = &(image->image[i][j]);
+            printf("%3d %3d %3d   ", (int)pixel->R, (int)pixel->G, (int)pixel->B);
+        }
+        pixel = &(image->image[i][j]);
+        printf("%3d %3d %3d\n", (int)pixel->R, (int)pixel->G, (int)pixel->B);
+    }
 }
 
 //Frees an image
 void freeImage(Image *image)
 {
-	//YOUR CODE HERE
+    uint32_t row = 0;
+    for (row = 0; row < image->rows; ++row) {
+        free(image->image[row]);
+    }
+    free(image->image);
+    free(image);
 }
