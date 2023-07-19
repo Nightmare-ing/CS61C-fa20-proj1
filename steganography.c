eda6222d79735a5d530743d503e1b85633c85d00
Qingsong Hu <kingsanhu@gmail.com>
2023-07-19 15:46:08 +0800

    Finish Part A2
    But don't know how to exit with code -1 when any ohter error occurs.
    Other errors like input is not correct or a malloc fails is added to the
    code

--- steganography.c
+++ steganography.c
@@ -21,13 +21,48 @@
 //Determines what color the cell at the given row/col should be. This should not affect Image, and should allocate space for a new Color.
 Color *evaluateOnePixel(Image *image, int row, int col)
 {
-	//YOUR CODE HERE
+    Color *result_color = (Color *) malloc(sizeof(Color));
+    if (result_color == NULL) exit(-1);
+
+	uint8_t B_value = image->image[row][col].B;
+    if (B_value & 1) {
+        result_color->R = 0;
+        result_color->G = 0;
+        result_color->B = 0;
+    } else {
+        result_color->R = 255;
+        result_color->G = 255;
+        result_color->B = 255;
+    }
+    return result_color;
 }
 
 //Given an image, creates a new image extracting the LSB of the B channel.
 Image *steganography(Image *image)
 {
-	//YOUR CODE HERE
+    Image *decoded_image = (Image *) malloc(sizeof(Image));
+    if (decoded_image == NULL) exit(-1);
+
+    Color *decoded_pixel = NULL;
+    uint32_t i = 0, j = 0, rows = image->rows, cols = image->cols;
+    decoded_image->image = (Color **) malloc(sizeof(Color *) * rows);
+    if (decoded_image->image == NULL) exit(-1);
+
+    decoded_image->rows = image->rows;
+    decoded_image->cols = image->cols;
+
+    for (i = 0; i < image->rows; ++i) {
+        Color *pixel_row = (Color *) malloc(sizeof(Color) * cols);
+        if (pixel_row == NULL) exit(-1);
+
+        for (j = 0; j < image->cols; ++j) {
+            decoded_pixel = evaluateOnePixel(image, i, j);
+            pixel_row[j] = *decoded_pixel;
+            free(decoded_pixel);
+        }
+        decoded_image->image[i] = pixel_row;
+    }
+    return decoded_image;
 }
 
 /*
@@ -45,5 +80,22 @@ Make sure to free all memory before returning!
 */
 int main(int argc, char **argv)
 {
-	//YOUR CODE HERE
+    Image *image = NULL;
+    Image *decoded_image = NULL;
+    char *filename;
+
+    if (argc != 2) {
+        printf("usage: %s filename\n", argv[0]);
+        printf("filename is an ASCII PPM file (type P3) with maximuum value 255.\n");
+        exit(-1);
+    }
+    filename = argv[1];
+
+    image = readData(filename);
+    decoded_image = steganography(image);
+    writeData(decoded_image);
+    freeImage(image);
+    freeImage(decoded_image);
+
+    return 0;
 }
